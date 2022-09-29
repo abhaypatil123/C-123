8555762be9183e6848ba87f7e4635baa50c18fe3
abhaypatil123 <108726063+abhaypatil123@users.noreply.github.com>
2022-09-29 21:19:39 +0530

    Add files via upload

---
+++ Virtual4.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int x,y;
+
+        virtual void fun()
+        {
+            cout<<"Base fun"<<"\n";
+        }
+         void gun()
+        {
+            cout<<"Base gun"<<"\n";
+        }
+};
+class Derived : public Base
+{
+    public:
+        int i,j;
+        void sun()
+        {
+            cout<<"Derived sun"<<"\n";
+        }
+        void fun()
+        {
+            cout<<"Derived fun"<<"\n";
+        }
+};
+int main()
+{
+    Base *bp = new Derived();   // Upcasting
+    bp->fun();
+    bp->gun();
+    //bp->sun();        // Not allowed
+
+    cout<<"Size of Base : "<<sizeof(Base)<<"\n";        // 12
+    cout<<"Size of Derived : "<<sizeof(Derived)<<"\n";  // 20
+    return 0;
+}
